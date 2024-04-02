57de57be148fa009c26685b4ded2fdf43a0c14fe
Vedant Yaduvanshi-) <87130565+vedantyaduvanshi@users.noreply.github.com>
2024-04-02 12:29:03 +0530

    Delete Frontend directory

--- Frontend/src/routes/LoggedInRoutes.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom';
-import HomePage from '../Pages/HomePage';
-import { useSelector } from 'react-redux';
-
-export default function LoggedInRoutes() {
-    const {user} = useSelector((state)=>({...state}));
-    return user ? <Outlet/> : <HomePage/>;
-}
